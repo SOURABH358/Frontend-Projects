ef82ace307f084c0e5bf5cc6aa159df08074a57b
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-08 18:50:33 +0530

    added files

--- frontend-project/src/App.js
+++ frontend-project/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import './App.css'
 import Header from "./Header";
 import Footer from "./Footer";
+import Main from "./Main";
 
 export default function App(){
     return <>
         <Header/>
+        <Main/>
         <Footer/>
     </>
 }
