64076ffd5bdd4806b1ca36427d0037302fb2a522
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-08 18:17:24 +0530

    added files

---
+++ frontend-project/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (<section className="header__section">
+        <title></title>
+        <div className="mode">
+            <div className="mode__switch"></div>
+        </div>
+    </section>)
+}
+export default Header
