0b4d87b2352d94c32739371dcd143d5a36215b6e
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-11 12:45:27 +0530

    added files

--- frontend-project/src/data.js
+++ frontend-project/src/data.js
@@ -58,91 +58,91 @@ const projects = [
     {
         id: '9',
         title: 'Remote work landing page',
-        image: './images/project-1.jpg',
+        image: './images/project-9.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-09/',
         tags: 'sass, javascript'
     },
     {
         id: '10',
         title: 'Expense tracker chart component',
-        image: './images/project-1.jpg',
+        image: './images/project-10.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-10/',
         tags: 'sass'
     },
     {
         id: '11',
         title: 'Order Summary Component',
-        image: './images/project-1.jpg',
+        image: './images/project-11.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-11/',
         tags: 'sass'
     },
     {
         id: '12',
         title: 'Room Home Page',
-        image: './images/project-1.jpg',
+        image: './images/project-12.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-12/',
         tags: 'css, sass, javascript'
     },
     {
         id: '13',
         title: '3-Column Card Component',
-        image: './images/project-1.jpg',
+        image: './images/project-13.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-13/',
         tags: 'sass'
     },
     {
         id: '14',
         title: 'Profile-Card-Component',
-        image: './images/project-1.jpg',
+        image: './images/project-14.jpg',
         link: 'https://timely-torte-74a5d9.netlify.app/',
         tags: 'sass'
     },
     {
         id: '15',
         title: 'Bookmark Landing Page',
-        image: './images/project-1.jpg',
+        image: './images/project-15.jpg',
         link: 'https://magenta-narwhal-dab150.netlify.app/',
         tags: 'sass'
     },
     {
         id: '16',
         title: 'Time Tracking Dashboard',
-        image: './images/project-1.jpg',
+        image: './images/project-16.jpg',
         link: 'https://musical-gelato-07fe55.netlify.app/',
         tags: 'tailwind'
     },
     {
         id: '17',
         title: 'Project preview card component',
-        image: './images/project-1.jpg',
+        image: './images/project-17.jpg',
         link: 'https://thriving-mandazi-5946cd.netlify.app/',
         tags: 'tailwind'
     },
     {
         id: '18',
         title: 'QR Code Generator',
-        image: './images/project-1.jpg',
+        image: './images/project-18.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-18/index.html',
         tags: 'css, javascript'
     },
     {
         id: '19',
         title: 'Email Validator',
-        image: './images/project-1.jpg',
+        image: './images/project-19.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-19/index.html',
         tags: 'css, javascript'
     },
     {
         id: '20',
         title: 'Speech To Text',
-        image: './images/project-1.jpg',
+        image: './images/project-20.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-20/index.html',
         tags: 'css, javascript'
     },
     {
         id: '21',
         title: 'News Homepage',
-        image: './images/project-1.jpg',
+        image: './images/project-21.jpg',
         link: 'https://sourabh358.github.io/100_projects.github.io/project-21/index.html',
         tags: 'css, javascript'
     }
